8c4c61648b1df03e16ce546f7e80058faa81a59f
Ben Haugen <ben1232jazz@hotmail.com>
2018-09-12 04:00:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 function shout(string) {
-  return string.toUpperCase();
+  return string.toUpperCase()
 }
 
 function whisper(string) {
-  return string.toLowerCase();
+  return string.toLowerCase()
 }
 
 function logShout(string) {
-  console.log("hello".toUpperCase());
+  console.log("hello".toUpperCase())
 }
 
 function logWhisper(string) {
-  console.log("hello".toLowerCase());
+  console.log("hello".toLowerCase())
 }
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase) 
-  return "I can't hear you!";
+  return "I can't hear you!"
   }
   
   if (string === string.toUpperCase) 
-  return "YES INDEED";
-
+  return "YES INDEED"
+  
   if (string === "I love you, Grandma") 
-  return "I love you, too.";
+  return "I love you, too."
 }
